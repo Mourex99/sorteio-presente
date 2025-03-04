9678cccd441f1400752a7603548321c5c4805b9a
Mateus Moura <mateusmoura.st@outlook.com>
2025-03-04 19:01:01 -0300

    .

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,13 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAwBzfiPyy0x5kVZD68bDsn7kPsOTF1R6w",
-    authDomain: "sorteio-presente.firebaseapp.com",
-    projectId: "sorteio-presente",
-    storageBucket: "sorteio-presente.firebasestorage.app",
-    messagingSenderId: "468128834933",
-    appId: "1:468128834933:web:282019808687d0ff3f9f3c"
-  };
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+};
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
